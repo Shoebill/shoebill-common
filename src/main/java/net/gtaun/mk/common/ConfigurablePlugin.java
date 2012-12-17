ee52d039db08aba84186b1dc7ecd35d886fe2cc3
mk124 <mk124a@gmail.com>
2012-12-17 08:02:24 +0800

    修改项目名为 mk-shoebill-common ，修改许可证为 git@github.com:GTAUN/mk-shoebill-common.git 许可证。
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/mk/common/ConfigurablePlugin.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2012 MK124
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.gtaun.mk.common;
+
+import net.gtaun.shoebill.object.Player;
+import net.gtaun.shoebill.resource.Plugin;
+
+/**
+ * 可被配置的插件抽象类。
+ * 
+ * @author MK124
+ */
+public abstract class ConfigurablePlugin extends Plugin
+{
+	protected ConfigurablePlugin()
+	{
+		
+	}
+	
+	public boolean configure(Player player)
+	{
+		return false;
+	}
+}
---
+++ src/main/java/net/gtaun/mk/common/dialog/AbstractInputDialog.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2012 MK124
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.gtaun.mk.common.dialog;
+
+import net.gtaun.shoebill.Shoebill;
+import net.gtaun.shoebill.constant.DialogStyle;
+import net.gtaun.shoebill.object.Player;
+import net.gtaun.util.event.EventManager;
+
+/**
+ * 抽象输入对话框类。
+ * 
+ * @author MK124
+ */
+public abstract class AbstractInputDialog extends AbstractDialog
+{
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager)
+	{
+		super(DialogStyle.INPUT, player, shoebill, rootEventManager);
+	}
+}
